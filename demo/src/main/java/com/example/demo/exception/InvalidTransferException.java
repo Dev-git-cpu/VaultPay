661356976d850b22d6f3b229efd54f353aa21d97
Dev Sonone <devesonone222@gmail.com>
2026-02-08 14:55:24 +0530

    Update code

--- demo/src/main/java/com/example/demo/exception/InvalidTransferException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class InvalidTransferException extends RuntimeException {
-    public InvalidTransferException(String message) {
-
-        super(message);
-    }
-}
